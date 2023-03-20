d59965b25d9f269f79303eead4e7ba9ec499d857
Bomi Lee <swedenebomi@gmail.com>
2023-03-20 12:05:56 +0100

    tailwind css with flex-row

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div class="flex flex-row ... ">
+    <div class="p-4 text-white bg-red-500 rounded">1</div>
+    <div class="p-4 text-white bg-red-500 rounded">2</div>
+    <div class="p-4 text-white bg-red-500 rounded">3</div>
+    <div class="p-4 text-white bg-red-500 rounded">4</div>
+    </div>
+    </>
+  );
+}
+
+export default App;
