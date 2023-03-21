a240b9ea97e84366613164453a0f84c2b5ec0ba8
Bomi Lee <swedenebomi@gmail.com>
2023-03-21 09:50:50 +0100

    use gap

--- src/App.js
+++ src/App.js
@@ -22,11 +22,28 @@ function App() {
     <input className='p-7  text-green-300 bg-slate-900 rounded-full'  type="text" placeholder='enter email '/>
     </div>
      
-     <div className='container mx-auto bg-gray-200 rounded-xl shadow border p-p m-10'>
+     <div className='container mx-auto bg-gray-200 rounded-xl shadow border p-3 m-10'>
       <p className='text-3xl text-gray-700 font-bold mb-5' > Study !</p>
       <p className='text-blue-700 text-lg'> React and Tailwind CSS in action </p>
      </div>
+
+     <div className='flex flex-wrap bg-pink-300 rounded-t-sm p-3 md:flex-wrap-reverse'>
+      <div>01</div>
+      <div>02</div>
+      <div>03</div>
+      </div>
   
+      <div class="flex gap-5 ...">
+  <div className="flex-none  bg-pink-400 w-14 h-14 ...">
+    01
+  </div>
+  <div className="grow   bg-pink-500 h-14 ...">
+    02
+  </div>
+  <div className="flex-none  bg-pink-600 w-14 h-14 ...">
+    03
+  </div>
+</div>
     </>
   );
 }
